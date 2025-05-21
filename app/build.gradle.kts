c04f04324a3b5841f91284611e319542458cdc5c
Kristina B <lena446699@gmail.com>
2025-05-21 03:02:31 +0300

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,19 @@ plugins {
     id("com.github.ben-manes.versions") version "0.52.0"
     id("application")
     id("checkstyle")
+    id("org.sonarqube") version "6.2.0.5505"
 }
 group = "App.java"
 version = "1.0-SNAPSHOT"
 application {
 	mainClass = "hexlet.code.App"
+}
+sonar {
+  properties {
+    property("sonar.projectKey", "KristinaB-t_java-project-61")
+    property("sonar.organization", "kristinab-t")
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
 }
 repositories {
     mavenCentral()
